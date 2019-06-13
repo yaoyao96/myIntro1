9c114d7d1f566bdb71496c813d939968ac610873
yaoyao96 <jyyaoyaoyao@gmail.com>
2019-06-13 23:09:18 +0800

    implement the 'workExperience'

---
+++ src/components/displayField.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="row q-mx-lg items-start">
+      <q-icon :name="iconName" class="col-2" :color="iconColor" style="font-size: 32px"></q-icon>
+      <p class="text-subtitle1">{{text}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'displayField',
+  props: {
+    iconName: String,
+    text: String,
+    iconColor: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/workExperience.vue
+++ src/pages/workExperience.vue
@@ -1,72 +1,49 @@
-<template>
-  <div class="q-px-lg q-mx-lg q-pb-md">
-    <q-timeline :layout="layout" color="secondary">
-      <q-timeline-entry heading>{{$t('bachelor.heading')}}</q-timeline-entry>
-      <q-timeline-entry
-        :title="$t('bachelor.schoolname')"
-        :subtitle="$t('bachelor.startdate')"
-        side="left"
-      >
-        <q-expansion-item
-          expand-separator
-          icon="fab fa-readme"
-          :label="$t('bachelor.major')"
-          :caption="$t('bachelor.degree')"
-        >
-          <q-card>
-            <q-card-section>
-              {{$t('bachelor.mainstuff')}}
-            </q-card-section>
-          </q-card>
-        </q-expansion-item>
-      </q-timeline-entry>
-
-      <q-timeline-entry
-        :title="$t('bachelor.end')"
-        :subtitle="$t('bachelor.graduatedate')"
-        side="right"
-        icon="fas fa-archway"
-      >
-      </q-timeline-entry>
-
-      <q-timeline-entry heading>{{$t('bachelor.heading')}}</q-timeline-entry>
-      <q-timeline-entry
-        :title="$t('bachelor.schoolname')"
-        :subtitle="$t('bachelor.startdate')"
-        side="left"
-      >
-        <q-expansion-item
-          expand-separator
-          icon="fab fa-readme"
-          :label="$t('bachelor.major')"
-          :caption="$t('bachelor.degree')"
-        >
-          <q-card>
-            <q-card-section>
-              {{$t('bachelor.mainstuff')}}
-            </q-card-section>
-          </q-card>
-        </q-expansion-item>
-      </q-timeline-entry>
-
-      <q-timeline-entry
-        :title="$t('bachelor.end')"
-        :subtitle="$t('bachelor.graduatedate')"
-        side="right"
-        color="orange"
-        icon="fas fa-archway"
-      >
-      </q-timeline-entry>
-    </q-timeline>
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+  <div>
+    <q-item class="row">
+      <q-item-section class="logo col-5">
+    <q-img src="./assets/KAPPS.png" class="bg-grey"></q-img>
+      </q-item-section>
+      <q-item-section class="col-7 q-gutter-sm-none q-gutter-md">
+        <display-field icon-name="face" text="place"></display-field>
+        <display-field icon-name="face" text="position"></display-field>
+        <display-field icon-name="face" text="date"></display-field>
+        <display-field icon-name="face" text="company brief"></display-field>
+      </q-item-section>
+    </q-item>
+    <div class="q-pa-lg card">ddd</div>
+    <br>
+    <q-separator inset />
+    <q-item>
+      <q-item-section>
+        <q-img src="./assets/kraftHeinzLogo.png" class="logo"></q-img>
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>Single line item</q-item-label>
+        <q-item-label caption>Secondary line text.</q-item-label>
+      </q-item-section>
+    </q-item>
   </div>
 </template>
 
 <script>
+import displayField from 'src/components/displayField'
+import DisplayField from '../components/displayField'
 export default {
-  computed: {
-    layout () {
-      return this.$q.screen.lt.sm ? 'dense' : (this.$q.screen.lt.md ? 'comfortable' : 'loose')
-    }
-  }
+  components: { DisplayField },
+  displayField
 }
 </script>
+
+<style>
+  .card {
+    width: 80vw;
+    margin: auto;
+    border-radius: 1em;
+    border: 1px solid grey;
+  }
+  .logo {
+    height: 200px;
+    width: 400px;
+  }
+</style>
