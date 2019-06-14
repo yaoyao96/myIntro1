4a141bc73b5d28c0f8fb9902a7a0dd131ec37de7
yaoyao96 <jyyaoyaoyao@gmail.com>
2019-06-14 23:53:21 +0800

    implement the 'workExperience'

---
+++ src/components/mainTaskCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-card>
+    <q-card-section><q-avatar color="primary" text-color="white">J</q-avatar></q-card-section>
+    <q-card-section>
+      <div class="text-h6">Our Changing Planet</div>
+      <div class="text-subtitle2">by John Doe</div>
+    </q-card-section>
+    <q-separator inset /></q-card>
+</template>
+
+<script>
+export default {
+  name: 'mainTaskCard'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/workExperience.vue
+++ src/pages/workExperience.vue
@@ -1,17 +1,28 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
-  <div>
+  <div class="q-pa-lg">
     <q-item class="row">
-      <q-item-section class="logo col-5">
-    <q-img src="./assets/KAPPS.png" class="bg-grey"></q-img>
+      <q-space></q-space>
+      <q-item-section class="logo col-4">
+    <q-img src="./assets/KAPPS.png"></q-img>
       </q-item-section>
       <q-item-section class="col-7 q-gutter-sm-none q-gutter-md">
-        <display-field icon-name="face" text="place"></display-field>
+        <display-field icon-name="face" iconColor="yellow" text="place"></display-field>
         <display-field icon-name="face" text="position"></display-field>
         <display-field icon-name="face" text="date"></display-field>
         <display-field icon-name="face" text="company brief"></display-field>
       </q-item-section>
     </q-item>
-    <div class="q-pa-lg card">ddd</div>
+    <div class="q-pa-lg card">
+      <!--<q-chip square color="grey" text-color="white" class="glossy q-py-md">-->
+        <!--<q-icon name="face" style="font-size: 32px"></q-icon>-->
+        <!--<h6 class="text-weight-light q-px-sm">Main Task</h6></q-chip>-->
+      <div class="row no-wrap q-gutter-md q-ma-sm">
+        <main-task-card></main-task-card>
+        <main-task-card></main-task-card>
+        <main-task-card></main-task-card>
+        <main-task-card></main-task-card>
+      </div>
+    </div>
     <br>
     <q-separator inset />
     <q-item>
@@ -29,8 +40,9 @@
 <script>
 import displayField from 'src/components/displayField'
 import DisplayField from '../components/displayField'
+import mainTaskCard from 'src/components/mainTaskCard'
 export default {
-  components: { DisplayField },
+  components: { DisplayField, mainTaskCard },
   displayField
 }
 </script>
@@ -39,8 +51,6 @@ export default {
   .card {
     width: 80vw;
     margin: auto;
-    border-radius: 1em;
-    border: 1px solid grey;
   }
   .logo {
     height: 200px;
